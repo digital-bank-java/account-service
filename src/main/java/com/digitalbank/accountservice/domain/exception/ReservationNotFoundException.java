3be01a4fb737b312630bcbcf4f3f73b75aa4fdb9
Rami Mohamed <rami.workstation@gmail.com>
2026-08-30 21:46:45 +0400

    feat: handle ledger posting outcomes idempotently

---
+++ src/main/java/com/digitalbank/accountservice/domain/exception/ReservationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.digitalbank.accountservice.domain.exception;
+
+public class ReservationNotFoundException extends RuntimeException {
+
+	public ReservationNotFoundException(String reservationRequestId) {
+		super("Reservation not found: " + reservationRequestId);
+	}
+}
